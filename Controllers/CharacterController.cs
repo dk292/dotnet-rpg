6e8362529dc0f82a26d5cf0326dc515824ef9a6e
Htet Hlaing Win <htethlaingwin.bdm29@gmail.com>
2023-05-11 16:52:06 +0630

    Modified Get Methods

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -10,12 +10,21 @@ namespace dotnet_rpg.Controllers
     [Route("api/[controller]")]
     public class CharacterController : ControllerBase
     {
-        private static Character knight = new Character();
+        private static List<Character> characters = new List<Character>(){
+            new Character(),
+            new Character {Id = 1 , Name = "Sam"}
+        };
         
-        [HttpGet]
+        [HttpGet("GetAll")]
 
-        public ActionResult<Character> Get() {
-            return Ok(knight);
+        public ActionResult<List<Character>> Get() {
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+
+        public ActionResult<Character> GetSingle(int id) {
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
         }
     }
 }
